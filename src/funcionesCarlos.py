76ef4a48493e7099f5923113ed97a93d7a0e22de
Carlos <7903808@alumnos.ufv.es>
2022-09-14 14:27:40 +0200

    Terminada la representacion de MiniSOM, empezando el GridSearch y aniadido un jupyter extra donde estan las funciones antiguas sin revisar que hay que ir aniadiendo

--- src/funcionesCarlos.py
+++ src/funcionesCarlos.py
@@ -1,31 +1,49 @@
-#####-------------------------------------------------------######
+#####-----------------------------------------------------------------------------------------------######
+#####-----------------------------------------------------------------------------------------------######
+#####-----------------------------------------------------------------------------------------------######
 """
-Archivo con funciones hechas por Carlos
+NOMBRE DEL ARCHIVO: "funcionesCarlos.py"
+AUTOR: CARLOS ARRANZ LUQUE
+FECHA: 2022
+DESCRIPCIÓN: ARCHIVO CON FUNCIONES DE PYHTON PARA EL PROYECTO DE EVALUACIÓN DE TÉCNICAS DE BALANCEO DEL CEIEC
 """
-#####-------------------------------------------------------######
-#####-------------------------------------------------------######
+#####-----------------------------------------------------------------------------------------------######
+#####-----------------------------------------------------------------------------------------------######
+#####-----------------------------------------------------------------------------------------------######
 
 
-#### LIBRERIAS 
+
+#####---------------------LIBRERIAS-----------------------------------------------------------------###### 
 
 from sklearn import preprocessing
 import pandas as pd
 
-#### FIN LIBRERIAS
+#####---------------------FIN LIBRERIAS-------------------------------------------------------------###### 
 
 
 
-#### FUNCIONES
+#####---------------------FUNCIONES-----------------------------------------------------------------###### 
 
 """
-   Normalización mediante la técnica minmax
+   Normalización mediante la técnica minmax de un data frame
 """
 def min_max(df):
+
+    nombres_columnas = df.columns 
+    # Guardo los nombres de las columnas para no perderlos ya que la función que usaremos sustituye los
+    # nombres de las columnas por números
     
-    #Usamos el MinMax que viene con SKLearn ---> from sklearn import preprocessing
+    # Usamos el MinMax que viene con SKLearn ---> from sklearn import preprocessing
     min_max_scaler = preprocessing.MinMaxScaler()
     x=df.values
     x_scaled = min_max_scaler.fit_transform(x)
     df = pd.DataFrame(x_scaled)
+
+    df.columns = nombres_columnas  # Devuelvo los nombres de las columnas
     
+    # Devolvemos el data frame ya normalizado
     return df
+#####-----------------------------------------------------------------------------------------------######
+
+
+#####---------------------FIN FUNCIONES-------------------------------------------------------------###### 
